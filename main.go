a60be46ce54c6b0e6e88ab3678bd72a5c2e4ad20
Induma Withanage <164716763+IndWit@users.noreply.github.com>
2025-11-02 19:50:53 +0530

    learn

--- main.go
+++ main.go
@@ -14,4 +14,32 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get Your ticket to attend")
 
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// ask user for their name
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - uint(userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", 
+	firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaininng for %v\n", remainingTickets, conferenceName)
+
+	// userName = "John Doe"
+	// userTickets = 2
+	// fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email shortly.\n", userName, userTickets)
+
 }
